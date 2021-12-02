e25e947f9cc57f7b955d46fdac4330b265418622
Mazed4D <playmyfiddleman@gmail.com>
2021-12-02 19:49:17 +0100

    Changed README.md, updated "add" page

--- src/components/ui/AddComponent.js
+++ src/components/ui/AddComponent.js
@@ -1,51 +1,74 @@
 import React from 'react';
 import { useState } from 'react';
-import ImageUploading from 'react-images-uploading';
+import styled from 'styled-components';
+
+const Image = styled.img`
+	width: 60%;
+	height: auto;
+	border-radius: 10px;
+
+	&:active,
+	:hover {
+		color: white;
+		transform: scale(1.1);
+	}
+	transition: all 0.1s ease-in;
+`;
+
+const ImageUpload = styled.div`
+	height: 100%;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 2rem;
+	label,
+	button {
+		border: none;
+		background-color: white;
+		padding: 1rem;
+		font-weight: 500;
+		border-radius: 10px;
+		cursor: pointer;
+		&:active,
+		:hover {
+			background-color: black;
+			color: white;
+			transform: scale(1.1);
+		}
+		transition: all 0.1s ease-in;
+	}
+	button {
+		font-weight: 800;
+	}
+`;
 
 const AddComponent = () => {
-	// const [images, setImages] = useState([]);
-	// const maxNumber = 69;
-	// const onChange = (imageList, addUpdateIndex) => {
-	// 	// data for submit
-	// 	console.log(imageList, addUpdateIndex);
-	// 	setImages(imageList);
-	// };
-	// return (
-	// 	<ImageUploading value={images} onChange={onChange} dataURLKey='data_url'>
-	// 		{({
-	// 			imageList,
-	// 			onImageUpload,
-	// 			onImageRemoveAll,
-	// 			onImageUpdate,
-	// 			onImageRemove,
-	// 			isDragging,
-	// 			dragProps,
-	// 		}) => (
-	// 			// write your building UI
-	// 			<div className='upload__image-wrapper'>
-	// 				<button
-	// 					style={isDragging ? { color: 'red' } : undefined}
-	// 					onClick={onImageUpload}
-	// 					{...dragProps}
-	// 				>
-	// 					Click or Drop here
-	// 				</button>
-	// 				&nbsp;
-	// 				<button onClick={onImageRemoveAll}>Remove image</button>
-	// 				{imageList.map((image, index) => (
-	// 					<div key={index} className='image-item'>
-	// 						<img src={image['data_url']} alt='' width='100' />
-	// 						<div className='image-item__btn-wrapper'>
-	// 							<button onClick={() => onImageUpdate(index)}>Update</button>
-	// 							<button onClick={() => onImageRemove(index)}>Remove</button>
-	// 						</div>
-	// 					</div>
-	// 				))}
-	// 			</div>
-	// 		)}
-	// 	</ImageUploading>
-	// );
-	return <></>;
+	const [image, setImage] = useState();
+
+	const changeHandler = (event) => {
+		setImage(URL.createObjectURL(event.target.files[0]));
+		console.info(event);
+	};
+
+	const submitImage = () => {};
+
+	return (
+		<ImageUpload>
+			<label htmlFor='image'>Choose image</label>
+			<input
+				type='file'
+				name='image'
+				id='image'
+				accept='image/png, image/jpeg'
+				onChange={changeHandler}
+				style={{ visibility: 'hidden' }}
+			/>
+			<Image src={image} alt='' />
+			{image && <button onClick={submitImage}>Submit image</button>}
+		</ImageUpload>
+	);
 };
 
 export default AddComponent;
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,3 +19,4 @@ const app = initializeApp(firebaseConfig);
 export default app;
 export const analytics = getAnalytics(app);
 export const auth = getAuth();
+export const database = getDatabase(app);
