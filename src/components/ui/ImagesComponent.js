180c297697420cb1171359cee421b61af659de1f
Mazed4D <playmyfiddleman@gmail.com>
2021-12-04 19:16:20 +0100

    added loading element, when images are pressed the user is navigated to all of that user's posts

--- src/components/ui/ImagesComponent.js
+++ src/components/ui/ImagesComponent.js
@@ -4,6 +4,9 @@ import { storage } from '../../firebase';
 import { getDownloadURL, ref, list } from 'firebase/storage';
 import styled from 'styled-components';
 import ProfileImage from './ProfileImage';
+import Loading from './Loading';
+import { useNavigate } from 'react-router';
+import { onValue } from 'firebase/database';
 
 const ImageBox = styled.div`
 	display: grid;
@@ -15,12 +18,16 @@ const ImageBox = styled.div`
 `;
 
 const ImagesComponent = ({ userId }) => {
+	const navigate = useNavigate();
 	const imagesRef = ref(storage, `/posts/${userId}`);
 	const [images, setImages] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [nextPageToken, setNextPageToken] = useState();
 	useEffect(() => {
 		let imageUrls = [];
 		const listFetch = async () => {
 			await list(imagesRef, { maxResults: 12 }).then((res) => {
+				setNextPageToken(res.nextPageToken);
 				res.items.forEach((item) => {
 					getDownloadURL(item).then((res) => {
 						imageUrls.push(res);
@@ -29,18 +36,58 @@ const ImagesComponent = ({ userId }) => {
 			});
 		};
 		listFetch();
+		setIsLoading(true);
 		setTimeout(() => {
 			setImages(imageUrls);
-		}, 400);
+			setIsLoading(false);
+		}, 700);
 	}, []);
 
+	const loadMore = async () => {
+		let imageUrls = [];
+		const listFetch = async () => {
+			await list(imagesRef, { maxResults: 12, pageToken: nextPageToken }).then(
+				(res) => {
+					setNextPageToken(res.nextPageToken);
+					res.items.forEach((item) => {
+						getDownloadURL(item).then((res) => {
+							imageUrls.push(res);
+						});
+					});
+				}
+			);
+		};
+		listFetch();
+		setIsLoading(true);
+		setTimeout(() => {
+			setImages((state) => {
+				return [...state, imageUrls];
+			});
+			setIsLoading(false);
+		}, 700);
+	};
+
+	const navigateToPosts = () => {
+		navigate(`/posts/${userId}`);
+	};
+
 	return (
-		<ImageBox>
-			{images.map((url, index) => {
-				console.log(url);
-				return <ProfileImage url={url} alt='sneed' key={index} />;
-			})}
-		</ImageBox>
+		<>
+			{isLoading && <h1>Loading...</h1>}
+			<ImageBox>
+				{images.map((url, index) => {
+					console.log(url);
+					return (
+						<ProfileImage
+							url={url}
+							alt='sneed'
+							key={index}
+							navigateToPosts={navigateToPosts}
+						/>
+					);
+				})}
+			</ImageBox>
+		</>
 	);
 };
 
