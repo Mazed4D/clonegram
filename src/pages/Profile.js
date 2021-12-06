ab21746f8750e4d6517975a70d9531e6b6d4bfa7
Mazed4D <playmyfiddleman@gmail.com>
2021-12-06 19:03:54 +0100

    changed styling, profile page now changes based on the url

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,27 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import placeholderImg from '../images/user.png';
 import styled from 'styled-components';
 import ImagesComponent from '../components/ui/ImagesComponent';
+import { useParams } from 'react-router';
+import { database } from '../firebase';
+import { ref as dbRef, get } from 'firebase/database';
 
 const UserInfo = styled.div`
 	display: flex;
-	margin: 0 1rem;
+	margin: 1rem 0.3rem;
+	height: 5rem;
 	justify-content: space-between;
+	align-items: center;
+`;
+
+const UserImage = styled.img`
+	border-radius: 50%;
+	height: 100%;
+	width: auto;
+	object-fit: cover;
+	border: 5px solid #006891;
+`;
+
+const UserName = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	height: 100%;
+	h3,
+	p {
+		margin: 0;
+	}
+`;
+
+const UserFollows = styled.div`
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+	flex-direction: column;
+	height: 100%;
+	p {
+		margin: 0;
+		padding: 0;
+	}
 `;
 
 const Profile = () => {
-	const { user } = useAuth();
+	const params = useParams();
+	const [userName, setUserName] = useState('User');
+
+	const fetchUser = async () => {
+		const path = `/users/${params.userId}`;
+		const ref = dbRef(database, path);
+		get(ref).then((snapshot) => {
+			if (snapshot.val() !== null) {
+				setUserName(snapshot.val());
+			}
+		});
+	};
+
+	useEffect(() => {
+		fetchUser();
+	}, []);
 
 	return (
 		<div>
 			<UserInfo className='userInfo'>
-				<img src={placeholderImg} alt='placeholder' />
-				<h2>{user.displayName}</h2>
-				<h3>0 Followers</h3>
-				<h3>0 Followings</h3>
+				<UserImage src={placeholderImg} alt='placeholder' />
+				<UserName>
+					<h3>{userName}</h3>
+					<p>{params.userId}</p>
+				</UserName>
+				<UserFollows>
+					<p>0</p>
+					<p>Followers</p>
+					<p>0</p>
+					<p>Followings</p>
+				</UserFollows>
 			</UserInfo>
-			<ImagesComponent userId={user.uid} />
+			<ImagesComponent userId={params.userId} />
 		</div>
 	);
 };
